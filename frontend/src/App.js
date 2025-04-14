982e62261e98b4cebf0b3811091900e6e511e790
Ratnakshi Gore <ratnakshisgore@gmail.com>
2025-04-13 21:35:15 -0600

    Add new ticket form created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 // import { Entry } from './page/entry/Entry.page';
 import DefaultLayout from "./layout/DefaultLayout";
-import Dashboard  from './page/dashboard/Dashboard.page';
- 
+// import Dashboard  from './page/dashboard/Dashboard.page';
+import {AddTicket} from "./page/new-ticket/AddTicket.page";
 
 
 function App() {
   return (
     <div className="App">
       <DefaultLayout >
-        <Dashboard/>
+        {/* <Dashboard /> */}
+        <AddTicket/>
         </DefaultLayout>
     </div>
   );
